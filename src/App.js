75a48f50299c43ccb1f249a6f1e6b6308ee079f6
praispranav <praispranav@gmail.com>
2021-02-27 17:45:42 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./styles.css";
+import React from "react";
+import Conditional from "./conditional";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: true
+    };
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        isLoading: false
+      });
+    }, 1000);
+  }
+  render() {
+    return (
+      <div className="App">
+        <Conditional isLoading={this.state.isLoading} />
+        <h1>Hello</h1>
+      </div>
+    );
+  }
+}
+export default App;
